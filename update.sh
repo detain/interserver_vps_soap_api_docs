72d6b3ecee586c2b59f6db48bd3a64feb02cc523
Joe Huss <detain@interserver.net>
2014-12-15 19:00:09 -0500

    updates

--- update.sh
+++ update.sh
@@ -1,2 +1,14 @@
-curl -X POST --data-urlencode content@README.md \
-http://documentup.com/compiled > index.html 
+#!/bin/bash
+# issues can also be a URL for our own site
+# travis for http://travis-ci.org/
+# theme can be v1 or default
+curl -# -X POST \
+  -d name="Interserver VPS API Documentation" \
+  -d color="#336699" \
+  -d twitter=interserver \
+  -d theme=default \
+  -d google_analytics="UA-32229521-1" \
+  -d travis=false \
+  -d issues=true \
+  --data-urlencode content@README.md \
+  http://documentup.com/compiled > apidoc.html
